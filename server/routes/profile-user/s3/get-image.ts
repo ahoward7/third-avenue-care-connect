b8a3afc26c563201b2b447110ef56d86f8d2e7c1
Avery Howard <avery.d.howard@gmail.com>
2025-04-09 18:48:00 -0400

    Change to AWS_S3_REGION

--- server/routes/profile-user/s3/get-image.ts
+++ server/routes/profile-user/s3/get-image.ts
@@ -2,7 +2,7 @@ import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_S3_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
